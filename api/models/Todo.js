4354fd767ce76f2d2c2f5bc586a344823c6411f0
litox06 <litox06@gmail.com>
2022-07-15 14:51:42 -0400

    Creating Mongoose model and exporting it

---
+++ api/models/Todo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({ // Creating To-do Schema
+    text: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
+    }
+})
+
+const Todo = mongoose.model("Todo", TodoSchema) // Creating the Mongoose model, naming it and passing the Schema
+
+module.exports = Todo;
